83c809f5eed1727c90baebacddea07e247f384b1
noureddine <noureddinelachgar9@gmail.com>
2022-11-30 01:18:51 +0100

    enhanced limits service

--- limits-service/src/main/java/com/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/microservices/limitsservice/controller/LimitsController.java
@@ -1,19 +1,25 @@
 package com.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservices.limitsservice.configuration.Configuration;
 import com.microservices.limitsservice.model.Limit;
 
 @RestController
 public class LimitsController {
 	
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	Limit retrieveLimits() {
+		
+		
 		return Limit.builder()
-				.maximum(6).
-				minimum(-2).
+				.maximum(configuration.getMaximum()).
+				minimum(configuration.getMinimum()).
 				build();
 	}
 
