7cb5a1d28f42ef84bd047b0e0da46ab02dcac3cc
noureddine <noureddinelachgar9@gmail.com>
2022-12-08 00:07:27 +0100

    add circuit breaker

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,18 @@
+package com.microservices.currencyexchangeservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+
+
+    @GetMapping("/sample-api")
+    public String sampleAPI() {
+        return "sample api";
+    }
+
+
+
+}
